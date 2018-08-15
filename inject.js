3f212b894d097773b066773ae0246358ef863594
Theophane vallaeys <webalorn@gmail.com>
2018-08-15 15:42:29 +0200

    Hide page during loading

--- inject.js
+++ inject.js
@@ -1,114 +1,130 @@
-function getZesteFilesJS() {
-	let zesteFiles = {
-		"logo" : "img/logo.png",
-		"ariane" : "img/ariane.png",
-		"menu" : "img/menu.png",
-		"piscine" : "img/piscine.jpg",
-		"crown" : "img/crown.png",
-		"menu_template" : "html/menu.html",
-		"formatBar_template" : "html/formatBar.html",
-		"offline_off" : "img/offline_off.png",
-		"offline_on": "img/offline_on.png",
-		"emojis": "img/emojis",
-	};
-	for (let name in zesteFiles) {
-		zesteFiles[name] = chrome.extension.getURL(zesteFiles[name]);
+var zBodyHiddenId = setInterval(function(){
+	if (document.body) {
+		document.body.style.visibility = "hidden";
+		window.clearInterval(zBodyHiddenId);
+	}
+}, 10);
+
+function startInjection() {
+	document.body.style.visibility = "hidden";
+	function getZesteFilesJS() {
+		let zesteFiles = {
+			"logo" : "img/logo.png",
+			"ariane" : "img/ariane.png",
+			"menu" : "img/menu.png",
+			"piscine" : "img/piscine.jpg",
+			"crown" : "img/crown.png",
+			"menu_template" : "html/menu.html",
+			"formatBar_template" : "html/formatBar.html",
+			"offline_off" : "img/offline_off.png",
+			"offline_on": "img/offline_on.png",
+			"emojis": "img/emojis",
+		};
+		for (let name in zesteFiles) {
+			zesteFiles[name] = chrome.extension.getURL(zesteFiles[name]);
+		}
+
+		return ['zesteFiles = ' + JSON.stringify(zesteFiles) + ';'].join('\n');
+	}
+	let zConfig;
+	function getZesteConfig() {
+		return ['zesteConfig = ' + JSON.stringify(zConfig) + ';'].join('\n');
 	}
 
-	return ['zesteFiles = ' + JSON.stringify(zesteFiles) + ';'].join('\n');
-}
-let zConfig;
-function getZesteConfig() {
-	return ['zesteConfig = ' + JSON.stringify(zConfig) + ';'].join('\n');
-}
+	let zModules = [
+		{
+			name: "Zeste datas",
+			js: [getZesteFilesJS, getZesteConfig],
+		},
+		{
+			name: "Core module",
+			js: ["modules/core/core.js"],
+		},
+		{
+			name: "Website base changes",
+			js: ["modules/fixes/fixes.js"],
+		},
+		{
+			name: "Forum text-editor",
+			js: ["modules/text_editor/edit_bar.js"],
+			css: ["modules/text_editor/edit_bar.css"],
+			require: ["text_editor"],
+		},
+		{
+			name: "Zeste style",
+			js: ["modules/zeste_style/zestes.js"],
+			css: ["modules/zeste_style/zestes.css"],
+			require: ["zeste"],
+		},
+		{
+			name: "Submisions style",
+			css: ["modules/submissions/submissions.css"],
+			require: ["submissions"],
+		},
+		{
+			name: "Task saver button",
+			css: ["modules/task_saver/button.css"],
+			js: ["modules/task_saver/button.js"],
+			internal: [taskSaverInternal],
+			require: ["task_saver"],
+		},
+		{
+			name: "Compact mode",
+			css: ["modules/compact/compact.css"],
+			js: ["modules/compact/compact.js"],
+			require: ["compact"],
+		},
+	];
 
-let zModules = [
-	{
-		name: "Zeste datas",
-		js: [getZesteFilesJS, getZesteConfig],
-	},
-	{
-		name: "Core module",
-		js: ["modules/core/core.js"],
-	},
-	{
-		name: "Website base changes",
-		js: ["modules/fixes/fixes.js"],
-	},
-	{
-		name: "Forum text-editor",
-		js: ["modules/text_editor/edit_bar.js"],
-		css: ["modules/text_editor/edit_bar.css"],
-		require: ["text_editor"],
-	},
-	{
-		name: "Zeste style",
-		js: ["modules/zeste_style/zestes.js"],
-		css: ["modules/zeste_style/zestes.css"],
-		require: ["zeste"],
-	},
-	{
-		name: "Submisions style",
-		css: ["modules/submissions/submissions.css"],
-		require: ["submissions"],
-	},
-	{
-		name: "Task saver button",
-		css: ["modules/task_saver/button.css"],
-		js: ["modules/task_saver/button.js"],
-		internal: [taskSaverInternal],
-		require: ["task_saver"],
-	},
-	{
-		name: "Compact mode",
-		css: ["modules/compact/compact.css"],
-		js: ["modules/compact/compact.js"],
-		require: ["compact"],
-	},
-];
+	config.get(function(configValues) {
+		zConfig = configValues;
+		let body = document.getElementsByTagName('body')[0];
+		let head = document.getElementsByTagName('head')[0];
 
-config.get(function(configValues) {
-	zConfig = configValues;
-	let body = document.getElementsByTagName('body')[0];
-
-	for (let mod of zModules) {
-		function injectThisMod() {
-			if (mod.js) {
-				for (let scriptContent of mod.js) {
-					let scriptDomEl = document.createElement('script');
-					if (typeof scriptContent == "string") {
-						scriptDomEl.src = chrome.extension.getURL(scriptContent);
-					} else { // function
-						scriptDomEl.textContent = scriptContent();
+		for (let mod of zModules) {
+			function injectThisMod() {
+				if (mod.js) {
+					for (let scriptContent of mod.js) {
+						let scriptDomEl = document.createElement('script');
+						if (typeof scriptContent == "string") {
+							scriptDomEl.src = chrome.extension.getURL(scriptContent);
+						} else { // function
+							scriptDomEl.textContent = scriptContent();
+						}
+						head.appendChild(scriptDomEl);
 					}
-					body.appendChild(scriptDomEl);
+				}
+				if (mod.css) {
+					for (let cssContent of mod.css) {
+						let link = document.createElement('link');
+						link.rel = "stylesheet";
+						link.type = "text/css";
+						link.href = chrome.extension.getURL(cssContent);
+						head.appendChild(link);
+					}
+				}
+				if (mod.internal) {
+					for (let func of mod.internal) {
+						func();
+					}
 				}
 			}
-			if (mod.css) {
-				for (let cssContent of mod.css) {
-					let link = document.createElement('link');
-					link.rel = "stylesheet";
-					link.type = "text/css";
-					link.href = chrome.extension.getURL(cssContent);
-					body.appendChild(link);
+			let canInject = true;
+			if (mod.require) {
+				for (let condition of mod.require) {
+					if (!configValues["enable_" + condition]) {
+						canInject = false;
+					}
 				}
 			}
-			if (mod.internal) {
-				for (let func of mod.internal) {
-					func();
-				}
+			if (canInject) {
+				injectThisMod();
 			}
 		}
-		let canInject = true;
-		if (mod.require) {
-			for (let condition of mod.require) {
-				if (!configValues["enable_" + condition]) {
-					canInject = false;
-				}
-			}
-		}
-		if (canInject) {
-			injectThisMod();
-		}
-	}
-});
+		$(function() {
+			console.log("rdm");
+			document.body.style.visibility = "visible";
+		});
+	});
+}
+document.addEventListener('DOMContentLoaded', startInjection, false);
