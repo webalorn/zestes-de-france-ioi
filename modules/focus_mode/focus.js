2a5a87a700964d5db6a7c8d792d4e1d9adfb6298
Sylvain Chiron <chironsylvain@orange.fr>
2019-04-08 23:06:06 +0200

    Don’t load focus mode on non-task pages
    Signed-off-by: Sylvain Chiron <chironsylvain@orange.fr>

--- modules/focus_mode/focus.js
+++ modules/focus_mode/focus.js
@@ -1,14 +1,16 @@
 $(function() {
-	let cell = $(".links-box-middle + td");
-	if ($("#taskSaved").length) {
-	   cell.width(140);
+	if ($("#task-tabs").length) {
+	   let cell = $(".links-box-middle + td");
+	   if ($("#taskSaved").length) {
+	      cell.width(140);
+      }
+      cell.prepend(
+		   $('<input type="checkbox" id="focusMode" /><label for="focusMode"><div></div><span>Mode sans distraction</span></label>')
+	   );
+	   let customCss = 'label[for="focusMode"] div { background-image: url("' + zesteFiles["focus"] + '"); }\n';
+	   $( "<style>" + customCss + "</style>" ).appendTo("head");
+	   $("#focusMode").change(function() {
+		   $(".menucol, .return-link-box, .arrowIcon")[this.checked ? 'fadeOut' : 'fadeIn']();
+	   });
    }
-   cell.prepend(
-		$('<input type="checkbox" id="focusMode" /><label for="focusMode"><div></div><span>Mode sans distraction</span></label>')
-	);
-	let customCss = 'label[for="focusMode"] div { background-image: url("' + zesteFiles["focus"] + '"); }\n';
-	$( "<style>" + customCss + "</style>" ).appendTo("head");
-	$("#focusMode").change(function() {
-		$(".menucol, .return-link-box, .arrowIcon")[this.checked ? 'fadeOut' : 'fadeIn']();
-	});
 });
